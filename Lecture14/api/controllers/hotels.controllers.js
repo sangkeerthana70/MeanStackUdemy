d80fb08d86bd1cced09c5c0e4144957a3206aab9
Anuradha <sangkeerthana@gmail.com>
2018-02-06 04:53:37 +0000

    Finished Lecture14

---
+++ Lecture14/api/controllers/hotels.controllers.js
@@ -0,0 +1,43 @@
+var hotelData = require('../data/hotel-data.json');
+
+module.exports.hotelsGetAll = function(req, res) {
+    console.log("Get the hotels");
+    console.log(req.query);//query string parameters.
+    
+    var offset = 0;
+    var count = 5;
+    
+    if(req.query && req.query.offset){
+        offset = parseInt(req.query.offset, 10);
+    }
+    
+    if(req.query && req.query.count){
+        count = parseInt(req.query.count, 10);
+    }
+    var returnData = hotelData.slice(offset, offset+count);
+    
+    res
+            .status(200)
+            .json(returnData);    
+};
+
+   
+module.exports.hotelsGetOne = function(req, res) {
+    var hotelId = req.params.hotelId;
+    var thisHotel = hotelData[hotelId];
+    console.log("Get hotelID", hotelId);
+        res
+            .status(200)
+            .json( thisHotel );//changing the res.send to a individual hotel instead of
+            //sending the whole hotelData as json object as we did before.
+};       
+
+module.exports.hotelsAddOne = function(req, res){
+    console.log("POST new hotel");
+    console.log(req.body);
+    res
+            .status(200)
+            .json(req.body);
+};
+            
+            
